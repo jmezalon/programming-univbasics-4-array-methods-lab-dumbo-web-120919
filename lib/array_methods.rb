2b7a938d03a4d3d8dc62ccbbefeeafff47c9cd08
Jean Max Mezalon <jeanmaxmezalon@pursuit.org>
2019-11-18 02:27:09 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,17 +1,17 @@
 def using_include(array, element)
-
+  array.include?(element)
 end
 
 def using_sort(array)
-
+  array.sort!
 end
 
 def using_reverse(array)
-
+  array.reverse!
 end
 
 def using_first(array)
-
+  array.first
 end
 
 def using_last(array)
@@ -19,5 +19,5 @@ def using_last(array)
 end
 
 def using_size(array)
-
+  array.length
 end
